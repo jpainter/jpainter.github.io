cdb4f9fa3bcee47224a7a4632d615dd3fee32261
John Painter <painter.ja@gmail.com>
2015-11-23 06:31:10 -0500

    cloned yihui's blog site

---
+++ setup blogging packages.R
@@ -0,0 +1,10 @@
+install.packages(c("knitr", "servr", "devtools"))     # To process .Rmd files
+devtools::install_github("hadley/lubridate")         # brocks reqs dev version
+devtools::install_github("brendan-r/brocks")         # My lazy wrapper funs
+
+# blogging
+# http://www.r-bloggers.com/blogging-with-rmarkdown-knitr-and-jekyll/
+
+library(brocks)
+new_post("My first blog post!")
+
